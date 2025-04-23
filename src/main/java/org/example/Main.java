53d20a50bab094987e66728f20c652c88f89867a
Varun Gole <varungole1999@gmail.com>
2025-04-23 17:01:14 -0400

    some more fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String sqlQuery = "CREATE TABLE EMPLOYEES (EmployeeName,EmployeeAge)";
-        Tokenizer tokenizer = new Tokenizer(sqlQuery);
-        String tokenized = tokenizer.start(sqlQuery);
-        Parser parser = new Parser(sqlQuery);
-        parser.parse();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+
+        while(flag) {
+            String sqlQuery = "CREATE TABLE EMPLOYEES (EmployeeName,EmployeeAge)";
+            Tokenizer tokenizer = new Tokenizer(sqlQuery);
+            String tokenized = tokenizer.start(sqlQuery);
+            Parser parser = new Parser(sqlQuery);
+            parser.parse();
+            System.out.println("DO you have more queries?");
+            int answer = sc.nextInt();
+            if(answer != 1) flag = false;
+        }
     }
 }
